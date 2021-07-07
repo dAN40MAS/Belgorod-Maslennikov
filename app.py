aaf7b371dd1e6051ba3c14fec7a867421283c0a8
dAN40MAS <74605706+dAN40MAS@users.noreply.github.com>
2021-07-07 17:48:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,16 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Article(db.Model):
-    __tablename__ = 'sqlite:///blog.db'
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     intro = db.Column(db.String(200), nullable=False)
     text = db.Column(db.Text, nullable=False)
-
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
 
     def __repr__(self):
         return '<Article %r>' % self.id
@@ -37,6 +31,15 @@ def about():
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+
+        db.session.add(article)
+        db.session.commit()
         return redirect('/')
     else:
         return render_template('create_article.html')
